11d5b2c2e2fb4fc0c20f4df78b689bcb73e7fde8
joamag <joamag@gmail.com>
2019-04-12 19:20:15 +0100

    New lint code

--- vue.js
+++ vue.js
@@ -2,6 +2,7 @@ module.exports = {
     extends: ["hive", "plugin:vue/essential", "plugin:vue/recommended"],
     plugins: ["vue"],
     rules: {
+        "vue/html-indent": ["error", 4],
         "vue/v-on-style": ["error", "longform"],
         "vue/v-bind-style": ["error", "longform"],
         "vue/max-attributes-per-line": [
