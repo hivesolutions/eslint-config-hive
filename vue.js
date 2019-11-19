11d3276ea3c786144e9a40382eca47c3a1751398
joamag <joamag@hive.pt>
2019-11-19 20:15:52 +0000

    Removed rule

--- vue.js
+++ vue.js
@@ -4,7 +4,6 @@ module.exports = {
     extends: ["hive", "plugin:vue/essential", "plugin:vue/recommended"],
     plugins: ["vue"],
     rules: {
-        "vue/no-v-html": ["off"],
         "vue/html-indent": ["error", 4],
         "vue/v-on-style": ["error", "longform"],
         "vue/v-bind-style": ["error", "longform"],
